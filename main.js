6e01cbc8dc1cde67747ec31497da424df80c46eb
Ankaleonova <ankaleonova@gmail.com>
2026-02-28 20:23:39 +0100

    variable fizz

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 for (let i = 1; i <= 100; i++){
     if (i % 3 === 0 && i % 5 === 0) {
          console.log("fizzbuzz");
-
     }
+    //multipla per 3
+      else if (i % 3 === 0) {
+        console.log("fizz");
        
     
