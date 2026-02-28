add30b750fa5e61e5b80690bd7beb4eb3f3a472a
Ankaleonova <ankaleonova@gmail.com>
2026-02-28 20:22:07 +0100

    add variable fizzbuzz

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 //numeri da 1 a 100
-for (let i = 1; i <= 100; i++) {
-    console.log(i);
-}
+for (let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+         console.log("fizzbuzz");
+
+    }
+       
+    
