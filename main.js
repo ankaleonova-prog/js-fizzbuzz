221642d2b1f1022b1278be7e98363a8722781223
Ankaleonova <ankaleonova@gmail.com>
2026-02-28 20:24:40 +0100

    add variable buzz

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ for (let i = 1; i <= 100; i++){
          console.log("fizzbuzz");
     }
     //multipla per 3
-      else if (i % 3 === 0) {
+    else if (i % 3 === 0) {
         console.log("fizz");
+    //multipla per 5
+    else if (i % 5 === 0) {
+        console.log("buzz");
        
     
