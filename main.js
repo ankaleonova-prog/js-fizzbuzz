75cd04d9f9ee8cf06ef736162732bec916e08aa1
Ankaleonova <ankaleonova@gmail.com>
2026-02-28 20:25:30 +0100

    add variable none

--- main.js
+++ main.js
@@ -6,8 +6,15 @@ for (let i = 1; i <= 100; i++){
     //multipla per 3
     else if (i % 3 === 0) {
         console.log("fizz");
+    }
     //multipla per 5
     else if (i % 5 === 0) {
         console.log("buzz");
+    }
+     else {
+        console.log(i);
+    }
+}
+
        
     
